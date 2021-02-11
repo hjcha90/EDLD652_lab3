3287bf201c86ee0939b61e0fee0dfb22be3bdff5
Rachael <rachaellatimer@gmail.com>
2021-02-10 22:17:24 -0800

    plot 1 edits

--- Lab3Script.R
+++ Lab3Script.R
@@ -1,4 +1,3 @@
-#install.packages("gghighlight")
 
 library(tidyverse)
 library(here)
@@ -11,31 +10,37 @@ library(forcats)
 library(lubridate)
 library(gghighlight)
 library(ggtext)
+library(albersusa)
 #library(colorblindr)
+
 
 hprod <- import(here("data", "honeyproduction.csv")) %>% 
   clean_names() %>% 
   as_tibble()
 
+#adjusted labels to where CA,  OR, WA end up after reordering
+#CA ends up at WY, OR at NY, WA at ND
 states <- unique(hprod$state)
-label_color <- ifelse(states == "CA" |states == "OR" | states == "WA", 
+label_color <- ifelse(states == "WY" |states == "NY" | states == "ND", 
                       "#C55644",
                       "gray30")
 
-label_face <- ifelse(states == "CA" |states == "OR" | states == "WA", 
+label_face <- ifelse(states == "WY" |states == "NY" | states == "ND", 
                      "bold",
                      "plain")
 
+    
 #plot1 heatmap needs scale or binning adjusted? too many values
 plot1 <- hprod %>% 
   mutate(production=totalprod/10000) %>% 
-  ggplot(aes(year, state))+
-  geom_tile(aes(fill=production), color="white")+
+  ggplot(aes(year, fct_reorder(state, production)))+
+  geom_tile(aes(fill=production))+
   scale_fill_viridis_c(option="magma")+
   scale_x_continuous(expand = c(0, 0)) +
-#next 2 lines not working
   theme(axis.text.y = element_text(color = label_color,
-                                  face = label_face))
+                                  face = label_face)) 
+
+  
 plot1 
 
 #plot2 is VERY hard to read; do not recommend using
